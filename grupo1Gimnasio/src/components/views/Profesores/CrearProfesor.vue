43a1b667c2bb92039d9c18525b5e335fd073f1e7
Gino Basile <basilegino@hotmail.com>
2023-05-23 20:22:52 -0300

    Agrego Crear Profesor

--- grupo1Gimnasio/src/components/views/Profesores/CrearProfesor.vue
+++ grupo1Gimnasio/src/components/views/Profesores/CrearProfesor.vue
@@ -1,19 +1,41 @@
-<script setup>
-
-
-</script>
+<template>
+  <div>
+    <h2>Crear Profesor</h2>
+    <div>
+      <strong>Nombre </strong>
+      <input class="m-2" type="text" v-model="profesor.nombre">
+      <br />
+      <strong>Apellido </strong>
+      <input class="m-2" type="text" v-model="profesor.apellido">
+   
+      <button class="m-3" @click="createProfesor">Crear Profesor</button>
+    </div>
+  </div>
+</template>
    
 <script>
-   
-    
+import { useElementStore } from '../../../stores/Common';
+import { useRouter } from 'vue-router'
+import { computed } from "vue";
+
+
+export default {
+  setup() {
+    const elementStore = useElementStore()
+    elementStore.setCurrentElement({ nombre: "", apellido: ""})
+    const profesor = computed(() => elementStore.currentElement);
+    const router = useRouter()
+    const url = "https://64662c65228bd07b355ddc69.mockapi.io/profesores";
+
+    const createProfesor = async () => {
+      await elementStore.createElement(url, profesor.value)
+      router.push('/profesores')
+    }
+
+    return {
+      createProfesor,
+      profesor
+    }
+  },
+}
 </script>
-
-
-
-<template>
-     <h1> Crear Profesor, REGISTER??? </h1>
-</template>
-
-
-<style scoped>
-</style>
