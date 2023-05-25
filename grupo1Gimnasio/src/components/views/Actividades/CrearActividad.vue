f949e2e25247aa3306034649d4616a44d1c95ca1
PedroKahane <84392542+PedroKahane@users.noreply.github.com>
2023-05-25 19:26:03 -0300

    actividades con select

--- grupo1Gimnasio/src/components/views/Actividades/CrearActividad.vue
+++ grupo1Gimnasio/src/components/views/Actividades/CrearActividad.vue
@@ -1,41 +1,75 @@
 <template>
-     <div>
-       <h2>Crear Paquete</h2>
-       <div>
-         <strong>Nombre </strong>
-         <input class="m-2" type="text" v-model="actividad.nombre">
-         <br />
-         <strong>Duracion </strong>
-         <input class="m-2" type="text" v-model="actividad.duracion">
-         <br />
-         <button class="m-3" @click="createActividad">Crear Actividad</button>
-       </div>
-     </div>
-   </template>
-      
-   <script>
-   import { useElementStore } from '../../../stores/Common';
-   import { useRouter } from 'vue-router'
-   import { computed } from "vue";
-   
-   
-   export default {
-     setup() {
-       const elementStore = useElementStore()
-       elementStore.setCurrentElement({ nombre: "", duracion: "", })
-       const actividad = computed(() => elementStore.currentElement);
-       const router = useRouter()
-       const url = "https://6460fabb491f9402f49bfa55.mockapi.io/Actividades";
-   
-       const createActividad = async () => {
-         await elementStore.createElement(url, actividad.value)
-         router.push('/actividades')
-       }
-   
-       return {
-         createActividad,
-         actividad
-       }
-     },
-   }
-   </script>
+  <div>
+    <h2>Crear Actividad</h2>
+    <div>
+      <strong>Nombre </strong>
+      <input class="m-2" type="text" v-model="actividad.nombre" />
+      <br />
+      <strong>Duracion </strong>
+      <input class="m-2" type="text" v-model="actividad.duracion" />
+      <br />
+      <p>
+        <strong>Sede: </strong>
+        <select v-model="actividad.idSede">
+          <option value="0" disabled selected>Selecciona la sede</option>
+          <option v-for="sede in sedes" :value="sede.idSede">
+            {{ sede.nombre }}
+          </option>
+        </select>
+      </p>
+      <p>
+        <strong>Profesor: </strong>
+        <select v-model="actividad.idProfesor">
+          <option value="0" disabled selected>Selecciona el profesor</option>
+          <option v-for="profesor in profesores" :value="profesor.id">
+            {{ profesor.nombre }}
+          </option>
+        </select>
+      </p>
+
+      <button class="m-3" @click="createActividad">Crear Actividad</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useElementStore } from '../../../stores/Common';
+import { useActividadStore } from '../../../stores/actividades';
+import { useRouter } from 'vue-router';
+import { onMounted, computed } from 'vue';
+
+export default {
+  setup() {
+    const elementStore = useElementStore();
+    const actividadStore = useActividadStore();
+    elementStore.setCurrentElement({
+      nombre: '',
+      duracion: '',
+      idSede: '',
+      idProfesor: '',
+    });
+    const actividad = computed(() => elementStore.currentElement);
+    const router = useRouter();
+    const url = 'https://6460fabb491f9402f49bfa55.mockapi.io/Actividades';
+
+    const createActividad = async () => {
+      await elementStore.createElement(url, actividad.value);
+      router.push('/actividades');
+    };
+
+    onMounted(() => {
+      actividadStore.fetchSedes();
+      actividadStore.fetchProfesores();
+    });
+
+    const sedes = computed(() => actividadStore.getSedes);
+    const profesores = computed(() => actividadStore.getProfesores);
+    return {
+      createActividad,
+      actividad,
+      sedes,
+      profesores,
+    };
+  },
+};
+</script>
--- grupo1Gimnasio/src/components/views/Actividades/DetalleActividad.vue
+++ grupo1Gimnasio/src/components/views/Actividades/DetalleActividad.vue
@@ -1,53 +1,82 @@
 <template>
-    <div>
-         <h2>Detalle de actividad</h2>
-         <div v-if="actividad">
-              <p>
-                   <strong>Nombre: </strong><input type="text" v-model="actividad.nombre" />
-              </p>
-              <p>
-                   <strong>Duracion(en minutos): </strong><input type="text" v-model="actividad.duracion" />
-              </p>
-           
-              <button @click="deleteActividad">Borrar actividad</button>
-              <button @click="updateActividad">Actualizar actividad</button>
-         </div>
+  <div>
+    <h2>Detalle de actividad</h2>
+    <div v-if="actividad">
+      <p>
+        <strong>Nombre: </strong
+        ><input type="text" v-model="actividad.nombre" />
+      </p>
+      <p>
+        <strong>Duracion(en minutos): </strong
+        ><input type="text" v-model="actividad.duracion" />
+      </p>
+      <p>
+        <strong>Sede: </strong>
+        <select v-model="actividad.idSede">
+          <option value="0" disabled selected>Selecciona la sede</option>
+          <option v-for="sede in sedes" :value="sede.idSede">
+            {{ sede.nombre }}
+          </option>
+        </select>
+      </p>
+      <p>
+        <strong>Profesor: </strong>
+        <select v-model="actividad.idProfesor">
+          <option value="0" disabled selected>Selecciona el profesor</option>
+          <option v-for="profesor in profesores" :value="profesor.id">
+            {{ profesor.nombre }}
+          </option>
+        </select>
+      </p>
+
+      <button @click="deleteActividad">Borrar actividad</button>
+      <button @click="updateActividad">Actualizar actividad</button>
     </div>
+  </div>
 </template>
 
 <script>
-import { useElementStore } from "../../../stores/Common";
-import { useRouter } from "vue-router";
-import { useRoute } from "vue-router";
-import { computed } from "vue";
+import { useElementStore } from '../../../stores/Common';
+import { useActividadStore } from '../../../stores/actividades';
+import { useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
+import { onMounted, computed } from 'vue';
 
 export default {
-    setup() {
-         const elementStore = useElementStore();
-         const router = useRouter();
-         const route = useRoute();
-         const actividadId = route.params.id.toString();
-         const url = "https://6460fabb491f9402f49bfa55.mockapi.io/Actividades";
+  setup() {
+    const elementStore = useElementStore();
+    const actividadStore = useActividadStore();
+    const router = useRouter();
+    const route = useRoute();
+    const actividadId = route.params.id.toString();
+    const url = 'https://6460fabb491f9402f49bfa55.mockapi.io/Actividades';
 
-         elementStore.fetchElementById(url, actividadId);
+    elementStore.fetchElementById(url, actividadId);
+    onMounted(() => {
+      actividadStore.fetchSedes();
+      actividadStore.fetchProfesores();
+    });
+    const actividad = computed(() => elementStore.currentElement);
 
-         const actividad = computed(() => elementStore.currentElement);
+    const sedes = computed(() => actividadStore.getSedes);
+    const profesores = computed(() => actividadStore.getProfesores);
+    const updateActividad = async () => {
+      await elementStore.updateElement(url, elementStore.currentElement);
+      router.push('/actividades');
+    };
 
-         const updateActividad = async () => {
-              await elementStore.updateElement(url, elementStore.currentElement);
-              router.push("/actividades");
-         };
+    const deleteActividad = async () => {
+      await elementStore.deleteElement(url, actividadId);
+      router.push('/actividades');
+    };
 
-         const deleteActividad = async () => {
-              await elementStore.deleteElement(url, actividadId);
-              router.push("/actividades");
-         };
-
-         return {
-            actividad,
-              updateActividad,
-              deleteActividad
-         };
-    },
+    return {
+      actividad,
+      updateActividad,
+      deleteActividad,
+      sedes,
+      profesores,
+    };
+  },
 };
 </script>
